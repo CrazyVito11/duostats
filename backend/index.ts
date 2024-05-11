cedadfc8d70359f56beff7360e9b0974f692b774
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2024-05-11 23:48:42 +0200

    Removed unused dotenv package from back-end

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,7 @@
 import express, { Application } from 'express';
-import dotenv from 'dotenv';
 import apiRoutes from './routes/api';
 import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
-// TODO: Remove .env support, as it doesn't seem to be neccesary
-dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 8000;
